dfb83a48f054b79de5dce6a653de3243bab6a64a
Abidemi Ajayi <demi.developer@gmail.com>
2022-09-11 04:57:52 -0700

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
- * Return (0)
+ * main - entry point
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
