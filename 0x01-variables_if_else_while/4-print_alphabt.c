0cd9833229cf3d45240e3c5172221cb92205125a
Abidemi Ajayi <demi.developer@gmail.com>
2022-09-12 19:28:31 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - code block
+ * Description: Display a-z except q and e
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - code block
+ * Description: print all base 10 number start from 0
+ * Return: 0
+ */
+int main(void)
+{
+char n = '0';
+if (n < '9')
+{
+putchar(n);
+ n++;
+}
+putchar('\n');
+return (0);
+}
