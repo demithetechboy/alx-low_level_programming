a0f4d9856c9df606fb931ee48e1c7044ad34b848
Abidemi Ajayi <demi.developer@gmail.com>
2022-09-12 17:00:03 -0700

    Updates

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,21 +2,20 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: Get a random number and print the number
- *and if it is positive, negative, or zero
- * Return: 0
- */
+* main - main block
+* Description: printing out the last random number stored in n variable
+* Return: 0
+*/
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
+if (n > 5)
+printf("%d and is greater than 5\n", n);
+else if (n < 6)
+printf("%d and is less than 6 and not 0\n", n);
 else
-printf("%d is zero\n", n);
-return (0);
+printf("%d and is 0\n", n);
+	return (0);
 }
