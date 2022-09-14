577fc38d70cd41576ffcd375b0f8c33fe89995c5
Abidemi Ajayi <demi.developer@gmail.com>
2022-09-14 10:19:47 -0700

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Print_to_98 - Print all natural numbers'
+ * in order
+ * @n: The number to brgin at .
+ * Return: 0;
+ */
+ void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("d%, ", n--");
+printf("d%\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
