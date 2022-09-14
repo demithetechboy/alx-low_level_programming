6ffc5a9121f35aa21282eedc0587618c3468eec1
Abidemi Ajayi <demi.developer@gmail.com>
2022-09-13 21:33:33 -0700

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - code block
+ * @c: display a-z
+ * Return: 0;
+ */
+int int _abs(int c)
+{
+if (c > 0 || c == 0)
+{
+return (c);
+}
+else
+{
+return (c * -1);
+}
+}
