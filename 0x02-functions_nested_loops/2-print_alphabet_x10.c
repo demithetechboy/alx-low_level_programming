a05062b81434b99010a3cecd583698055c4be79d
Abidemi Ajayi <demi.developer@gmail.com>
2022-09-13 21:00:31 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - code block
+ * print_alphabet_x10 - code block
  * print_alphabet: display a-z
  * Return: 0;
  */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 char c, i;
 for (i = 0; i < 10; i++)
