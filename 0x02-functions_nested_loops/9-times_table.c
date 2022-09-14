fb4c77f32d70f235be3e004793a9cbbf075072c2
Abidemi Ajayi <demi.developer@gmail.com>
2022-09-14 10:02:53 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ int num, mult, prod;
 for (num = 0; num <= 9; num++)
 {
 _putchar('0');
-for (mult = 1; mult <= 1; mult++)
+for (mult = 1; mult <= 9; mult++)
 {
 _putchar(',');
 _putchar(' ');
@@ -18,8 +18,8 @@ prod = num * mult;
 if (prod <= 9)
 _putchar(' ');
 else
-_putchar((prod / 10) + '0'));
-_putchar((prod % 10) + '0'));
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
