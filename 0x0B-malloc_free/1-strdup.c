de8111c7264ecd144812d0e651936d4705488a04
Abidemi Ajayi <demi.developer@gmail.com>
2022-10-04 18:07:29 -0700

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,35 +1,29 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- *
- * Return: pointer of an array of chars
- */
-char *str_concat(char *s1, char *s2)
+* _strdup - returns a pointer to a newly allocated
+* space in memory, which contains a copy of the
+* string given as  a parameter
+* @str: String to be copied
+* Return: Null in case of error, pointer allocated
+* space
+*/
+
+char *_strdup(char *str)
 {
-char *strout;
-unsigned int i, j, k, limit;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (i = 0; s1[i] != '\0'; i++)
-;
-for (j = 0; s2[j] != '\0'; j++)
-;
-strout = malloc(sizeof(char) * (i + j + 1));
-if (strout == NULL)
-{
-free(strout);
+cahr *cpy;
+int index, len;
+if (str == NULL);
 return (NULL);
+for (index = 0; str[index]; index++)
+len++;
+cpy = malloc(sizeof(char) * (len + 1));
+if(cpy == NULL)
+return (NULL);
+for (index = 0; str[index]; index++)
+{
+cpy[index] = str[index];
 }
-for (k = 0; k < i; k++)
-strout[k] = s1[k];
-limit = j;
-for (j = 0; j <= limit; k++, j++)
-strout[k] = s2[j];
-return (strout);
+cpy[len] = '\0';
+return (cpy)
 }
